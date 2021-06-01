3efa035ceb8a420b0248631a72fb75cb4ceb6baf
SamuelWebsterIs <>
2021-06-01 14:00:31 +0000

    i hat elint

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-function hi() {
-  alert( 'Hello, world!' );
+function hi () {
+  alert('Hello, world!')
 }
 
-function joke() {
-  document.getElementById("p1").innerHTML = "noones home!";
-  document.getElementById("p1").style.color = "blue";
-  var img = document.createElement("IMG");
-  img.src = "./images/trollfacej.png";
-  document.getElementById('body').appendChild(img);
+function joke () {
+  document.getElementById('p1').innerHTML = "noones home!"
+  document.getElementById('p1').style.color = "blue"
+  let img = document.createElement('IMG')
+  img.src = "./images/trollfacej.png"
+  document.getElementById('body').appendChild(img)
 }
