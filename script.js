e3eace3d6b682157470698b9833109bcf1958133
SamuelWebsterIs <>
2021-06-01 14:19:02 +0000

    stuff

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function hi () {
 }
 
 function joke () {
-  document.getElementById('p1').innerHTML = "noones home!"
-  document.getElementById('p1').style.color = "blue"
-  let img = document.createElement('IMG')
-  img.src = "./images/trollfacej.png"
+  document.getElementById('p1').innerHTML = 'noones home!'
+  document.getElementById('p1').style.color = 'blue'
+  const img = document.createElement('IMG')
+  img.src = './images/trollfacej.png'
   document.getElementById('body').appendChild(img)
 }
